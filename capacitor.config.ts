575f343cdaf4642c7382c32be8097c05913f499d
Gunabh Sharan <gunabhsharan.25@gmail.com>
2024-12-24 04:05:23 +0530

    refactor: remove unused Android files, update Capacitor configuration, and modify Next.js output settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.stp.app',
   appName: 'stp',
-  webDir: 'export'
+  webDir: '/.next',
 };
 
 export default config;
