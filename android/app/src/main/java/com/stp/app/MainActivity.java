6ad5b31f9c47514ae60608db4eea69e561a7731a
Gunabh Sharan <gunabhsharan.25@gmail.com>
2024-12-24 03:56:49 +0530

    refactor: set up Android project structure, integrate Capacitor, and configure Gradle settings

---
+++ android/app/src/main/java/com/stp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.stp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.stp.app',
   appName: 'stp',
-  webDir: 'public'
+  webDir: 'export'
 };
 
 export default config;
